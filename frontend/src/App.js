536a6ff382dbcc93d668cb18b4a6336de86a9270
Vincent <vshan0816@gmail.com>
2023-11-27 11:36:30 -0500

    add frontend auth routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,8 +3,8 @@ import { AuthRoute, ProtectedRoute } from './components/Routes/Routes';
 import NavBar from './components/NavBar/NavBar';
 
 import MainPage from './components/MainPage/MainPage';
-import LoginForm from './components/SessionForms/LoginForm';
-import SignupForm from './components/SessionForms/SignupForm';
+import LoginForm from './components/SessionForms/LoginForm.js';
+import SignupForm from './components/SessionForms/SignupForm.js';
 
 function App() {
   return (
