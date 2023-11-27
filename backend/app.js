2e22b567e259de0c12b43cb99655335da977c791
Omar <ohammad@gwu.edu>
2023-11-27 11:36:03 -0500

    add Express Server Security

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,14 @@ const express = require("express");
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
+const cors = require('cors');
+const csurf = require('csurf');
+const { isProduction } = require('./config/keys');
+
+
 const usersRouter = require('./routes/api/users');
 const quizzesRouter = require('./routes/api/quizzes');
+const csrfRouter = require('./routes/api/csrf');
 
 const app = express();
 
@@ -12,7 +18,22 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false })); 
 app.use(cookieParser());
 
+if (!isProduction) {
+    app.use(cors());
+}
+
+app.use(
+    csurf({
+        cookie: {
+            secure: isProduction,
+            sameSite: isProduction && "Lax",
+            httpOnly: true
+        }
+    })
+);
+
 app.use('/api/users', usersRouter);
 app.use('/api/quizzes', quizzesRouter); 
+app.use('/api/csrf', csrfRouter);
 
 module.exports = app;
