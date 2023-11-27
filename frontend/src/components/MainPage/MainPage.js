7a4bd959ed020ea8ad2cb2c01a4d20d340b65428
Vincent <vshan0816@gmail.com>
2023-11-27 14:41:47 -0500

    styling user auth

--- frontend/src/components/MainPage/MainPage.js
+++ frontend/src/components/MainPage/MainPage.js
@@ -1,11 +1,14 @@
+import './MainPage.css'
+
 function MainPage() {
     return (
-      <>
+      <div className="main">
         <p>Socrates</p>
+        <p>The Unexamind Life is Not Worth Living</p>
         <footer>
           Copyright &copy; 2023 Socrates
         </footer>
-      </>
+      </div>
     );
   }
   
