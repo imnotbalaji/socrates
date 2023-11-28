dcd0d8822024d785fd497d307c3173d7e92a70f3
Omar <ohammad@gwu.edu>
2023-11-28 10:17:30 -0500

    add get routes for quizzes

--- backend/models/Quiz.js
+++ backend/models/Quiz.js
@@ -4,38 +4,30 @@ const Schema = mongoose.Schema;
 const optionSchema = new Schema({
     optionOne: {
         type: String,
-        required: true
     },
     optionTwo: {
         type: String,
-        required: true
     },
      optionThree: {
         type: String,
-         required: true
     },
     optionFour: {
         type: String,
-        required: true
     }
 });
 
 const questionSchema = new Schema({
     question: {
         type: String,
-        required: true
     },
     options: {
         optionsArray: [optionSchema],
-        required: true
     },
     answer: {
         type: String,
-        required: true
     },
     response: {
         type: String,
-        required: true
     }
 });
 
--- backend/routes/api/quizzes.js
+++ backend/routes/api/quizzes.js
@@ -1,11 +1,47 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const Quiz = mongoose.model('Quiz');
 
-/* GET tweets listing. */
-router.get('/', function (req, res, next) {
-    res.json({
-        message: "GET /api/quizzes"
+// POST /api/quizzes/create
+router.post('/create', async (req, res, next) => {
+    debugger
+    const newQuiz = new Quiz({
+        questionsArray: req.body.questionsArray
     });
+
+    const quiz = await newQuiz.save();
+
+    return res.json(quiz)
+    // res.json(req.body)
+});
+
+//GET /api/quizzes/:id
+router.get('/:id', async (req, res, next) => {
+    try {
+        const quiz = await Quiz.findById(req.params.id)
+            .populate("questionsArray");
+        return res.json(quiz);
+    }
+    catch (err) {
+        const error = new Error('Quiz not found');
+        error.statusCode = 404;
+        error.errors = { message: "No quiz found with that id" };
+        return next(error);
+    }
+});
+
+/* GET quizzes listing. */
+router.get('/', async (req, res) => {
+    try {
+        const quizzes = await Quiz.find()
+            .populate("questionsArray")
+            // .sort({ createdAt: -1 });
+        return res.json(quizzes);
+    }
+    catch (err) {
+        return res.json([]);
+    }
 });
 
 module.exports = router;
