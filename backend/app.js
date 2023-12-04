256ce1289d3715a48173f3627f8d440e504942fd
Balaji <imnotbalaji@gmail.com>
2023-12-04 13:03:38 -0500

    Production v2- shifting some lines in entry file - moving things

--- backend/app.js
+++ backend/app.js
@@ -45,25 +45,6 @@ app.use('/api/users', usersRouter);
 app.use('/api/quizzes', quizzesRouter); 
 app.use('/api/csrf', csrfRouter);
 
-app.use((req, res, next) => {
-    const err = new Error('Not Found');
-    err.statusCode = 404;
-    next(err);
-});
-
-const serverErrorLogger = debug('backend:error');
-
-app.use((err, req, res, next) => {
-    serverErrorLogger(err);
-    const statusCode = err.statusCode || 500;
-    res.status(statusCode);
-    res.json({
-        message: err.message,
-        statusCode,
-        errors: err.errors
-    })
-});
-
 if (isProduction) {
   const path = require('path');
   // Serve the frontend's index.html file at the root route
@@ -85,6 +66,39 @@ if (isProduction) {
     );
   });
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.statusCode = 404;
+    next(err);
+});
+
+const serverErrorLogger = debug('backend:error');
+
+app.use((err, req, res, next) => {
+    serverErrorLogger(err);
+    const statusCode = err.statusCode || 500;
+    res.status(statusCode);
+    res.json({
+        message: err.message,
+        statusCode,
+        errors: err.errors
+    })
+});
 
 
 
