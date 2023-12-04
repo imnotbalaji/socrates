f4c98cc58ca7befb2b560a1c40942cbd8559ddd1
Balaji <imnotbalaji@gmail.com>
2023-12-04 12:58:36 -0500

    Production v2- shifting some lines in entry file

--- backend/app.js
+++ backend/app.js
@@ -24,27 +24,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false })); 
 app.use(cookieParser());
 
-if (isProduction) {
-    const path = require('path');
-    // Serve the frontend's index.html file at the root route
-    app.get('/', (req, res) => {
-      res.cookie('CSRF-TOKEN', req.csrfToken());
-      res.sendFile(
-        path.resolve(__dirname, '../frontend', 'build', 'index.html')
-      );
-    });
-  
-    // Serve the static assets in the frontend's build folder
-    app.use(express.static(path.resolve("../frontend/build")));
-  
-    // Serve the frontend's index.html file at all other routes NOT starting with /api
-    app.get(/^(?!\/?api).*/, (req, res) => {
-      res.cookie('CSRF-TOKEN', req.csrfToken());
-      res.sendFile(
-        path.resolve(__dirname, '../frontend', 'build', 'index.html')
-      );
-    });
-  }
 
 app.use(passport.initialize());
 
@@ -84,6 +63,34 @@ app.use((err, req, res, next) => {
         errors: err.errors
     })
 });
+
+if (isProduction) {
+  const path = require('path');
+  // Serve the frontend's index.html file at the root route
+  app.get('/', (req, res) => {
+    res.cookie('CSRF-TOKEN', req.csrfToken());
+    res.sendFile(
+      path.resolve(__dirname, '../frontend', 'build', 'index.html')
+    );
+  });
+
+  // Serve the static assets in the frontend's build folder
+  app.use(express.static(path.resolve("../frontend/build")));
+
+  // Serve the frontend's index.html file at all other routes NOT starting with /api
+  app.get(/^(?!\/?api).*/, (req, res) => {
+    res.cookie('CSRF-TOKEN', req.csrfToken());
+    res.sendFile(
+      path.resolve(__dirname, '../frontend', 'build', 'index.html')
+    );
+  });
+}
+
+
+
+
+
+
 
 module.exports = app;
 
