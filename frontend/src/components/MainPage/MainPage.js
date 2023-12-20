906e8d702049866dba4508fb513afd5fd08444c7
Omar <ohammad@gwu.edu>
2023-12-20 15:19:17 -0500

    styling changes

--- frontend/src/components/MainPage/MainPage.js
+++ frontend/src/components/MainPage/MainPage.js
@@ -12,11 +12,16 @@ function MainPage() {
         <div className = "main-image">
         </div>
         <div className = "main-text"> 
-        <h1>Socrates</h1>
-        <p>The Unexamined Life is Not Worth Living</p>
+          <h1>Socrates</h1>
+          <p>The Unexamined Life is Not Worth Living</p>
+          <p>Welcome to Socrates!  A learning aid that allows you to create a quiz on any topic, at either a beginner, intermediate or advanced level.  Login or signup to take your quiz!</p>
         </div>
       </div>
       <LoginForm/>
+      </div>
+      <div className='footer'>
+        <p>This App was Made Using | Open AI API | MongoDB | Render | Node.js | Express.js | React | Redux</p>
+        <p>Frontend Lead: Vincent Shan | Backend Lead: Omar Hammad | Team Lead: Balaji V</p>
       </div>
       
       </>
